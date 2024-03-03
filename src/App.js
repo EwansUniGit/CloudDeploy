f1987409008db92df5c394ed09db80381f26d25b
Ewan <Bobby@Nobby.com>
2024-03-03 14:32:06 +0000

    Inital push to GitHub

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
-
+import Button from 'react-bootstrap/button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 const App = () => {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
 
   useEffect(() => {
     axios.get('http://localhost:3001/tasks')
-        // .then(response => setTasks(response.data))
-        .then(response => console.log('hey baby'))
+        .then(response => setTasks(response.data))
         .catch(error => console.error('Error fetching tasks: ', error))
   });
+
 
   const addTask = () => {
     axios.post('http://localhost:3001/tasks', {description: newTask})
@@ -18,8 +22,7 @@ const App = () => {
         .catch(error => console.error('Error adding task: ', error))
   };
 
-  const toggletask = (taskId, completed) => {
-    // Update the completion status of a to-do item on the server
+  const toggleTask = (taskId, completed) => {
     axios.put(`http://localhost:3001/tasks/${taskId}`, { completed: !completed })
         .then(response => {
           const updatedTasks = tasks.map(task => (task.task_id === (taskId -1) ? response.data : task));
@@ -38,20 +41,22 @@ const App = () => {
         .catch(error => console.error('Error deleting todo:', error));
   };
   return (
-      <div>
-        <h1>To-Do App</h1>
-        <input type="text" value={newTask} onChange={e => setNewTask(e.target.value)} />
-        <button onClick={addTask}>Add task</button>
+      <Container>
+        <Row><h1>To-Do App</h1></Row>
+          <Row><input type="text" value={newTask} onChange={e => setNewTask(e.target.value)} /></Row>
+        <Button variant={"primary"} onClick={addTask}>Add task</Button>
         <ul>
           {tasks.map(task => (
+              <Row>
               <li key={task.task_id}>
-                <span style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>{task.description}</span>
-                <button onClick={() => toggletask(task.task_id, task.completed)}>Toggle</button>
-                <button onClick={() => deleteTask(task.task_id)}>Delete</button>
+                <span style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>{task.task_description}</span>
+                <Button variant={"success"} onClick={() => toggleTask(task.task_id, task.completed)}>Toggle</Button>
+                <Button variant={"secondary"} onClick={() => deleteTask(task.task_id)}>Delete</Button>
               </li>
+              </Row>
           ))}
         </ul>
-      </div>
+      </Container>
   );
 };
 
